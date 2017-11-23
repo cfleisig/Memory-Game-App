0a0cec1bf03d18ed3f8466108dcf4b50583d61dc
Clara Fleisig <ClaraFleisig@Claras-MacBook-Air.local>
2017-11-22 20:08:41 -0500

    Initial Commit

---
+++ Fleisig_Clara_memoryGameTests/Fleisig_Clara_memoryGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fleisig_Clara_memoryGameTests.swift
+//  Fleisig_Clara_memoryGameTests
+//
+//  Created by Clara Fleisig on 2017-11-22.
+//  Copyright © 2017 Clara Fleisig. All rights reserved.
+//
+
+import XCTest
+@testable import Fleisig_Clara_memoryGame
+
+class Fleisig_Clara_memoryGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
